4947e43e0d43691730645f22cf9513afbe78276a
Amanda Iverson <amanda@codefellows.com>
2017-09-12 14:13:50 -0700

    This is my lab for this git test

---
+++ lab01-amanda/MyNewConsoleApp/MyNewConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
