59d9e97338f069e56e8d633129dbe5351615ac07
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-12 12:26:29 -0700

    all questions and counter finished

--- lab01-tom/lab01-tom/Program.cs
+++ lab01-tom/lab01-tom/Program.cs
@@ -4,15 +4,9 @@ namespace lab01_tom
 {
     class Program
     {
-        //int[] questions = new int[0];
-
         static void Main()
         {
             NameQuestion();
-            Question1();
-            Question2();
-            Question3();
-            Question4();
             Total();
             Console.Read();
         }
@@ -33,75 +27,86 @@ namespace lab01_tom
             }
         }
 
-        static void Question1()
+        static int Question1()
         {
             Console.WriteLine("My current favorite game is Overwatch. Yes or No?");
-            string game = Console.ReadLine();
-            if (game == "yes" || game == "Yes")
+            string game = Console.ReadLine().ToLower();
+            int counter = 0;
+            if (game == "yes" || game == "y")
             {
                 Console.WriteLine("Yes it is. D.Va is the best!");
-                
+                counter++;
             }
             else
             {
                 Console.WriteLine("Wrong, it is!");
             }
+            return counter;
         }
 
-        static void Question2()
+        static int Question2()
         {
             Console.WriteLine("Out of the following, who is my favorite member of the Justice League?");
             string[] heroesArray = new string[] { "Batman", "Superman", "Wonder Woman", "The Flash"};
+            int counter = 0;
 
             foreach (string heroes in heroesArray)
             {
                 Console.WriteLine(heroes);
             }
 
-            string hero = Console.ReadLine();
+            string hero = Console.ReadLine().ToLower();
 
-            if (hero == "Batman" || hero == "batman")
+            if (hero == "batman")
             {
                 Console.WriteLine("That's right!");
+                counter++;
             }
             else
             {
                 Console.WriteLine("Wrong, it's Batman!");
             }
+            return counter;
         }
 
-        static void Question3()
+        static int Question3()
         {
             Console.WriteLine("I drive a Jeep. Yes or No?");
-            string car = Console.ReadLine();
-            if (car == "No" || car == "no")
+            string car = Console.ReadLine().ToLower();
+            int counter = 0;
+            if (car == "no" || car == "n")
             {
                 Console.WriteLine("Correct. I actually drive a Volkswagen.");
+                counter++;
             }
             else
             {
                 Console.WriteLine("No, I don't, but Jeeps are cool!");
             }
+            return counter;
         }
 
-        static void Question4()
+        static int Question4()
         {
             Console.WriteLine("I have a sister. Yes or No?");
-            string sister = Console.ReadLine();
-            if (sister == "Yes" || sister == "yes")
+            string sister = Console.ReadLine().ToLower();
+            int counter = 0;
+            if (sister == "yes" || sister == "y")
             {
                 Console.WriteLine("Yep, I have a younger sister.");
+                counter++;
             }
             else
             {
                 Console.WriteLine("I do! I have a younger sister.");
             }
+            return counter;
         }
 
         static void Total()
         {
-            //int numberCorrect = questions.Length;
-            //Console.WriteLine($"Thanks for playing! You got {numberCorrect} questions correct.");
+            int numberCorrect = Question1() + Question2() + Question3() + Question4();
+            Console.WriteLine($"Thanks for playing!. You got {numberCorrect} questions correct!");
         }
     }
 }   
