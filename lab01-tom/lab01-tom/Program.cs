42b1779cc02ef92662f315a39893caba98c7f348
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-12 09:27:12 -0700

    git add .

---
+++ lab01-tom/lab01-tom/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace lab01_tom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameQuestion();
+            IterationStatements();
+            Console.Read();
+        }
+
+        static void IterationStatements()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            int[] numberArray = new int[] { 72, 42, 21, 38 };
+            foreach (int number in numberArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            int z = 0;
+            while (z < 10)
+            {
+                Console.WriteLine(z);
+                z++;
+            }
+
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            while (n < 5);
+        }
+
+        static void NameQuestion()
+        {
+            Console.WriteLine("What is your name?");
+            string userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput.Trim()))
+            {
+                Console.WriteLine("Please type a name!");
+
+                NameQuestion();
+            }
+            else
+            {
+                Console.WriteLine($"Welcome {userInput}, let's play a game!");
+            }
+        }
+    }
+}
