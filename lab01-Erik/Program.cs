94deb4450c719e7be01e295b4cbf81e2884c5825
drkrieger <eplyushko@gmail.com>
2017-09-12 13:02:10 -0700

    added question 2

--- lab01-Erik/Program.cs
+++ lab01-Erik/Program.cs
@@ -8,32 +8,58 @@ namespace lab01_Erik
         static void Main(string[] args)
         {
             Console.WriteLine("This is Eriks about me quiz");
-            int questionsRight = Counter(0);
-            VideoGames();
-            Console.WriteLine($"you got {questionsRight}/3 right");
+            int questionsRight = 0;
+            questionsRight += VideoGames();
+            bool car = true;
+            while (car)
+            {
+                MyCar();
+                if (MyCar() == 1)
+                {
+                    car = false;
+                }
+            }
+            Console.WriteLine($"you got {questionsRight}/2 right");
             Console.Read();
-        }
-        static int Counter(int num)
-        {
-            int corect = 0;
-            corect += num; 
-            return corect;
-        }
-        static void VideoGames()
+        }       
+        static int VideoGames()
         {
             Console.WriteLine("Does Erik Like VideoGames?.. (yes or no)");
             String userAnswer = Console.ReadLine();
            
             if (userAnswer == "YES" || userAnswer == "yes")
             {
-               Counter(1);
+               
                 Console.WriteLine("good work you got it right!..");
+                return 1;
             }
             else
             {
                 Console.WriteLine("Nope Sorry wrong");
+                return 0;
             }
         
+        }
+        static int MyCar()
+        {
+            string[] carName = new string[3] { "NISSAN", "TOYOTA", "FORD"};
+            Console.WriteLine("What company made Erik's car?");
+            string userAnswer = Console.ReadLine().ToUpper();
+            int answer = 0;
+            foreach (string make in carName)
+            {
+                if(userAnswer == make)
+                {
+                    Console.WriteLine("nice work that is correct");
+                    answer = 1; 
+                }
+                else
+                {
+                    Console.WriteLine("sorry try again");
+                    answer = 0; 
+                }
+            }
+            return answer; 
         }
    
     }
