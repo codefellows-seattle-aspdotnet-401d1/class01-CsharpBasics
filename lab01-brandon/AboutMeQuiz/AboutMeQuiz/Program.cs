c59e996c15a937eb126ef78f8bfc5bd9c062caa1
Brandon Little <brlittle86@gmail.com>
2017-09-12 08:58:16 -0700

    added first question and answer accumulation

--- lab01-brandon/AboutMeQuiz/AboutMeQuiz/Program.cs
+++ lab01-brandon/AboutMeQuiz/AboutMeQuiz/Program.cs
@@ -13,6 +13,7 @@ namespace AboutMeQuiz
         {
             Console.WriteLine("Hello! My name is Brandon. Would you like to take a quiz about me?");
             string response = Console.ReadLine().ToLower();
+
             if (response == "y" || response == "yes")
             {
                 Console.WriteLine("Great! Let's get started:");
@@ -21,6 +22,35 @@ namespace AboutMeQuiz
             {
                 Console.WriteLine("Too bad, we're doing it anyway!");
             }
+
+            int correctAnswersTotal = QuizOne();
+
+            Console.WriteLine($"You got {correctAnswersTotal} questions correct!");
+            Console.Read();
+        }
+
+        static int QuizOne()
+        {
+            Console.WriteLine("Was I born in Washington?");
+            string response = Console.ReadLine().ToLower();
+            int accumulator = 0;
+
+            if (response == "y" || response == "yes")
+            {
+                Console.WriteLine("Wrong! I was born in Arizona.");
+            }
+            else if (response == "n" || response == "no")
+            {
+                Console.WriteLine("Correct! I was born in Arizona.");
+                accumulator++;
+            }
+            else
+            {
+                Console.WriteLine("I didn't understand your response, please try again.");
+                QuizOne();
+            }
+
+            return accumulator;
         }
     }
 }
