f3107775ad445e9359a208072dc6add0d9ec08dd
Brandon Little <brlittle86@gmail.com>
2017-09-12 08:47:28 -0700

    Created MainQuizHandler to ask user if they would like to start the quiz

--- lab01-brandon/AboutMeQuiz/AboutMeQuiz/Program.cs
+++ lab01-brandon/AboutMeQuiz/AboutMeQuiz/Program.cs
@@ -6,7 +6,21 @@ namespace AboutMeQuiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainQuizHandler();
+        }
+
+        static void MainQuizHandler()
+        {
+            Console.WriteLine("Hello! My name is Brandon. Would you like to take a quiz about me?");
+            string response = Console.ReadLine().ToLower();
+            if (response == "y" || response == "yes")
+            {
+                Console.WriteLine("Great! Let's get started:");
+            }
+            else if (response == "n" || response == "no")
+            {
+                Console.WriteLine("Too bad, we're doing it anyway!");
+            }
         }
     }
 }
